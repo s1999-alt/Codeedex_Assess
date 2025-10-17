204da5887f91ee4543a4d157efd38b04cbd08877
Siyad Savad <siyadsavad313@gmail.com>
2025-10-17 15:28:46 +0530

    models added for User and Post

--- backend/blogproject/api/models.py
+++ backend/blogproject/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+  email = models.EmailField(unique=True)
+
+  REQUIRED_FIELDS = ['email']
+
+
+class Post(models.Model):
+  title = models.CharField(max_length=255)
+  content = models.TextField()
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+  created_at = models.DateTimeField(auto_now_add=True)
+  likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
+
+  def __str__(self):
+    return self.title
+  
+
--- backend/blogproject/blogproject/settings.py
+++ backend/blogproject/blogproject/settings.py
@@ -12,6 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+AUTH_USER_MODEL = 'api.User'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
