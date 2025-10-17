3f62b050521355ef19c12c89c727a8793311e53a
Siyad Savad <siyadsavad313@gmail.com>
2025-10-17 15:35:59 +0530

    Comment model added

--- backend/blogproject/api/models.py
+++ backend/blogproject/api/models.py
@@ -20,3 +20,9 @@ class Post(models.Model):
     return self.title
   
 
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  message = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  
